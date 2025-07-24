8349ab45d3b416d6a9c3a5bcb1d9bf728e0cc44c
danielscos <70877551+danielscos@users.noreply.github.com>
2025-07-25 00:33:08 +0300

    Change logout to use button_to form instead of link for proper CSRF handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [ :destroy ]
-
   def new
     # Login form - no logic needed, just renders the form
   end
